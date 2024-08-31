01366b171e949bbf218b953a51ccb1add1296b5a
eldarmoore <eldar.moore@gmail.com>
2024-08-31 14:50:22 +0300

    Write PHP Like a Pro: Build a PHP MVC Framework From Scratch

--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <?php
 
-require "src/controllers/products.php";
+$action = $_GET["action"];
+$controller = $_GET["controller"];
 
-$controller = new Products;
+if ($controller === "products") {
 
-$action = $_GET['action'];
+    require "src/controllers/products.php";
+
+    $controller_object = new Products;
+
+} elseif ($controller === "home") {
+
+    require "src/controllers/home.php";
+
+    $controller_object = new Home;
+
+}
 
 if ($action === "index") {
-    $controller->index();
+
+    $controller_object->index();
+
 } elseif ($action === "show") {
-    $controller->show();
+
+    $controller_object->show();
+
 }
