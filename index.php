e6c1df07ea69ba8b1ef894c2666ba815d8554499
eldarmoore <eldar.moore@gmail.com>
2024-08-31 14:57:46 +0300

    Remove Repetition by Dynamically Creating Objects and Running Methods

--- index.php
+++ index.php
@@ -3,26 +3,8 @@
 $action = $_GET["action"];
 $controller = $_GET["controller"];
 
-if ($controller === "products") {
+require "src/controllers/$controller.php";
 
-    require "src/controllers/products.php";
+$controller_object = new $controller();
 
-    $controller_object = new Products;
-
-} elseif ($controller === "home") {
-
-    require "src/controllers/home.php";
-
-    $controller_object = new Home;
-
-}
-
-if ($action === "index") {
-
-    $controller_object->index();
-
-} elseif ($action === "show") {
-
-    $controller_object->show();
-
-}
+$controller_object->$action();
