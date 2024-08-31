c6da4d50b7065e22acc45c1ba296b3ef709d9c5d
eldarmoore <eldar.moore@gmail.com>
2024-08-31 13:47:34 +0300

    Add Another Page: How NOT to Do it in an MVC Framework

---
+++ views/products_show.php
@@ -0,0 +1,14 @@
+<!-- views/products_show.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Details</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+<h1>Product Details</h1>
+<p>Details of the product will be shown here.</p>
+
+</body>
+</html>
