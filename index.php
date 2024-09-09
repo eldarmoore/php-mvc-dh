a53d13eca94372ba25d0561555e6924d8ed6a253
eldarmoore <eldar.moore@gmail.com>
2024-09-09 16:03:47 +0300

    Use the Controller and Action from the Matched Route

--- index.php
+++ index.php
@@ -14,20 +14,13 @@ $router->add("/", ["controller" => "home", "action" => "index"]);
 // Match the current path to a route
 $params = $router->match($path);
 
-// For testing, print the matched parameters and exit
-var_dump($params);
-exit;
-
-// If matched, proceed with extracting controller and action
-$segments = explode('/', $path);
-
-// Get the controller and action from the URL
-$action = $segments[2];
-$controller = $segments[1];
+// Get the controller and action from the matched route
+$controller = $params["controller"];
+$action = $params["action"];
 
 // Require and instantiate the controller
 require "src/controllers/$controller.php";
 $controller_object = new $controller();
 
-// Call the action method
+// Call the action method on the controller object
 $controller_object->$action();
