01eb3ed49148ef201f027186d29eea6849202ac2
eldarmoore <eldar.moore@gmail.com>
2024-10-30 13:55:31 +0200

    Add a Render Method and Use it from the Controller

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -15,7 +15,8 @@ class Products
 
         $viewer = new Viewer();
 
-        require "views/products_index.php";
+        $viewer->render("products_index.php", $products);
+
     }
 
     public function show(string $id)
--- src/Framework/Viewer.php
+++ src/Framework/Viewer.php
@@ -4,5 +4,9 @@ namespace Framework;
 
 class Viewer
 {
+    public function render(string $template, array $products)
+    {
+        require "views/$template";
 
+    }
 }
