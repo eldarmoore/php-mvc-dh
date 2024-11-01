234b36fb09381fafd8d9dc1624dc7c751efc3262
eldarmoore <eldar.moore@gmail.com>
2024-11-01 19:17:40 +0200

    Add Shared View Templates for Common Code

--- views/Products/show.php
+++ views/Products/show.php
@@ -1,11 +1,3 @@
-<!-- views/products_show.php -->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Product Details</title>
-    <meta charset="UTF-8">
-</head>
-<body>
 
 <h1>Product Details</h1>
 <p>Show the product with ID <?= $id ?></p>
