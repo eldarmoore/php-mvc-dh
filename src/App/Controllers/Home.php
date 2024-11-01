ff2eb3be341070ca5c86b6cae78e4704d5c323aa
eldarmoore <eldar.moore@gmail.com>
2024-11-01 19:22:10 +0200

    Add a Title Variable to the Shared Header Template

--- src/App/Controllers/Home.php
+++ src/App/Controllers/Home.php
@@ -10,6 +10,10 @@ class Home
     {
         $viewer = new Viewer;
 
+        echo $viewer->render("shared/header.php", [
+            "title" => "Home",
+        ]);
+
         echo $viewer->render('Home/index.php');
     }
 }
--- views/Home/index.php
+++ views/Home/index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-    <meta charset="UTF-8">
-</head>
-<body>
 
 <h1>Welcome to the Homepage!</h1>
 <p>This is the homepage of the application.</p>
