b426dc58d1752aaafd86e3a484d128203444b0fa
eldarmoore <eldar.moore@gmail.com>
2024-11-09 09:39:34 +0200

    Process the Edit Form and Validate the Data

--- src/App/Controllers/Products.php
+++ src/App/Controllers/Products.php
@@ -99,4 +99,34 @@ class Products
             ]);
         }
     }
+
+    public function update(string $id)
+    {
+        $product = $this->model->find($id);
+
+        if ($product === false) {
+            throw new PageNotFoundException("Product not found");
+        }
+
+        $data = [
+            "name" => $_POST["name"],
+            "description" => empty($_POST["description"]) ? null : $_POST["description"]
+        ];
+
+        if ($this->model->update($id, $data)) {
+
+            header("Location: /products/{$id}/show");
+            exit;
+
+        } else {
+            echo $this->viewer->render("shared/header.php", [
+                "title" => "Edit Product"
+            ]);
+
+            echo $this->viewer->render("Products/edit.php", [
+                "errors" => $this->model->getErrors(),
+                "product" => $product
+            ]);
+        }
+    }
 }
